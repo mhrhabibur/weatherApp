cf0ab9f7b1b1607301ee9ee69e14f0d4fe024948
Habibur Rahman <mhr.habibur@gmail.com>
2022-02-07 21:44:14 +0600

    Location access update

--- Weather App/HomeViewController.swift
+++ Weather App/HomeViewController.swift
@@ -6,20 +6,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class HomeViewController: UIViewController, UITextFieldDelegate {
+class HomeViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
     
     @IBOutlet var CityLabel: UILabel!
     @IBOutlet var weatherCondition: UIImageView!
     @IBOutlet var searchTextField: UITextField!
     @IBOutlet var cityNameLabel: UILabel!
     
+    var locationManager: CLLocationManager!
+    
 
     var weatherManager = WeatherManager()
+    
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
         
     }
 
@@ -62,6 +74,35 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
         }
         
         searchTextField.text = ""
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        if  latitude == locValue.latitude &&
+                longitude == locValue.longitude {
+            return
+        }
+        latitude = locValue.latitude
+        longitude = locValue.longitude
+        
+        weatherManager.fetchDataAutomatically(latitude: latitude, longitude: longitude) { weather, error in
+            if let myWeather = weather {
+                
+                let temp = myWeather.main.temp
+                let city = String(myWeather.name)
+                let conditionID = myWeather.weather[0].id
+                
+                let wet = WeatherModel(cityName: city, temperature: temp, conditionId: conditionID)
+                let temperature = String(format:"%.1f", wet.temperature)
+                DispatchQueue.main.async {
+                    self.cityNameLabel.text = "\(temperature)°C"
+                    self.weatherCondition.image = UIImage(systemName: wet.weatherCondition)
+                    self.CityLabel.text = String(wet.cityName)
+                }
+            }
+        }
     }
 }
 
--- Weather App/WeatherManager.swift
+++ Weather App/WeatherManager.swift
@@ -35,5 +35,33 @@ class WeatherManager {
     
     
     
+    
+    
+    func fetchDataAutomatically(latitude: Double, longitude: Double, completion: @escaping (WeatherMapping?, Error?) -> Void) {
+        
+       if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=\(latitude)&lon=\(longitude)&appid=1f462dda351a1f36c3d8b29280ef0fdb")
+        {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {
+                return
+            }
+            if let data = data {
+                do {
+                let weather = try JSONDecoder().decode(WeatherMapping.self, from: data)
+
+                    completion(weather, nil)
+
+                   
+                } catch {
+                    completion(nil, error)
+                }
+            }
+        }.resume()
+       }
+        
+    }
+    
+    
+    
 }
 
